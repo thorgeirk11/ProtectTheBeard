d8a8d34301ff1ac9d17f989537cbb43ef047839b
Þorgeir <thorgeir@radiantgames.is>
2017-03-24 19:42:29 +0000

    more code

---
+++ Assets/BeardController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeardController : MonoBehaviour {
+
+    public GameObject beard;
+    public List<Transform> beardparts;
+    public static BeardController instance;
+    public bool Dead { get; private set; }
+    // Use this for initialization
+    void Awake()
+    {
+        beardparts = new List<Transform>();
+        Dead = false;
+        foreach (Transform item in beard.transform)
+        {
+            beardparts.Add(item);
+        }
+        instance = this;
+    }
+    void Start () {
+        
+
+	}
+
+    public void RemovePartOfBeard(int howMany)
+    {
+        for(int i = beardparts.Count-1; i >= 0; i--)
+        {
+            if (howMany == 0) break;
+            if (beardparts[i].GetComponent<Renderer>().enabled)
+            {
+                beardparts[i].GetComponent<Renderer>().enabled = false;
+                howMany--;
+            }
+        }
+        if (howMany > 0) Dead = true;
+    }
+
+}
